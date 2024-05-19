0ce0d3bfc92438ab6b1bba3432ba89624d25e2aa
quackquavk <pukarkhanal4@gmail.com>
2024-05-19 07:16:48 +0545

    pukar

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/images/logo.png"
+        <meta
+          property="og:title"
+          content="Paradiso Pokhara - Sports bar and grill"
         />
+        <meta
+          property="og:description"
+          content="Experience an unforgettable dining adventure with great food and live music that keeps the night alive in a vibrant, family-friendly environment. Our restaurant not only offers an immaculate setting but also boasts an exceptionally friendly staff and unparalleled hospitality. Nestled just behind our premises, enjoy the breathtaking view of Lake Fewa. Our extensive menu showcases the finest Indian, Italian, Chinese, and Mexican cuisines, ensuring a delightful culinary journey. Complement your meal with our wide array of drinks. It's the ultimate destination for gatherings and family dining, promising an extraordinary experience that will leave you coming back for more."
+        />
+        <meta
+          property="og:image"
+          content="/images/logo.png"
+        />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="icon" type="image/png" href="/images/logo.png" />
       </Head>
       <body className={forum.className}>{children}</body>
     </html>
