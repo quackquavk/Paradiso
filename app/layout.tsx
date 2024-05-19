c9c50dcffc42a69f7ce849b1304aecdd46d15e74
quackquavk <pukarkhanal4@gmail.com>
2024-05-19 07:21:52 +0545

    pukar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description:
     "Experience an unforgettable dining adventure with great food and live music that keeps the night alive in a vibrant, family-friendly environment. Our restaurant not only offers an immaculate setting but also boasts an exceptionally friendly staff and unparalleled hospitality. Nestled just behind our premises, enjoy the breathtaking view of Lake Fewa. Our extensive menu showcases the finest Indian, Italian, Chinese, and Mexican cuisines, ensuring a delightful culinary journey. Complement your meal with our wide array of drinks. It's the ultimate destination for gatherings and family dining, promising an extraordinary experience that will leave you coming back for more. ",
   icons: {
-    icon: "favicon.ico",
+    icon: "/images/favicon.ico",
   },
 };
 
@@ -33,7 +33,7 @@ export default function RootLayout({
           content="/images/logo.png"
         />
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link rel="icon" type="image/png" href="/images/logo.png" />
+        <link rel="icon" type="image/ico" href="/images/favicon.ico" />
       </Head>
       <body className={forum.className}>{children}</body>
     </html>
